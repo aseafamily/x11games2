56c17ef3cce8541a6b8cf7eb60d1d1524fbb3711
Andrew <andrewma11@outlook.com>
2025-07-20 18:15:54 -0700

    Hangman games

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def read_index():
-    html_content = ""
-    # Check if index.html exists in the current directory
-    if os.path.exists("index.html"):
-        with open("index.html", "r") as f:
-            html_content = f.read()
-    else:
-        # Fallback content if index.html is not found
-        html_content = "<html><body><h1>index.html not found</h1></body></html>"
-    return html_content
+    return render_template('index.html')
+
+@app.route('/games/hangman')
+def hangman():
+    return render_template('games_hangman2.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000) 
+
+    
